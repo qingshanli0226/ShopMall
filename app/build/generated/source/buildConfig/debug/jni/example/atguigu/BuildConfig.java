8d253e7e5a223c33e55af169503b2c23b4fda3e2
bryan sun <hitsjt@gmail.com>
2019-10-16 16:39:10 +0800

    Signed-off-by: bryan sun <hitsjt@gmail.com> 16:39

---
+++ app/build/generated/source/buildConfig/debug/jni/example/atguigu/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package jni.example.atguigu;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "jni.example.atguigu";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
