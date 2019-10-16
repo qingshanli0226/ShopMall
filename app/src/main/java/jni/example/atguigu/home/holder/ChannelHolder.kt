66e4ef368c2f9df6ea7fc98e2be7f91aa2305961
Your Name <you@example.com>
2019-10-16 19:40:43 +0800

    sshh

--- app/src/main/java/jni/example/atguigu/home/holder/ChannelHolder.kt
+++
@@ -1,7 +0,0 @@
-package jni.example.atguigu.home.holder
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class ChannelHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-}
--- app/src/main/java/jni/example/atguigu/utils/Retorfit.kt
+++
@@ -1,7 +0,0 @@
-package jni.example.atguigu.utils
-
-import retrofit2.http.POST
-
-interface Retorfit {
-
-}
